ba3a8371e8d762495798b19be0589dbdb8256a07
neelesh <neeleshyadavyadav4702@gmail.com>
2023-04-05 18:11:32 +0530

    modification

--- src/setupproxy.js
+++ src/setupproxy.js
@@ -1,7 +1,7 @@
 const proxy = require("http-proxy-middleware");
 module.exports = function(app) {
   app.use(proxy("/api/**", { // https://github.com/chimurai/http-proxy-middleware
-    target: "https://localhost:4000",
+    target: "https://blogsite-6vx5.onrender.com",
     secure: false
   }));
 };
